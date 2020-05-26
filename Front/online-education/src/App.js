aa1166bcb5eb4ee7cac5a237d64862f50b63df74
Hossein <hossein.dehghanipour1998@gmail.com>
2020-05-26 14:38:56 +0430

    Added SignIn Form

--- Front/online-education/src/App.js
+++ Front/online-education/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import SignIn from './Components/SignIn/SignIn'
 import './App.css';
+// Material Design Bootstrap
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<div>
+		<SignIn />
+  	</div>
+    
   );
 }
 
